2f821e7822a86933488283cc8da431d4288c9232
IAmParadox <confusedstudent13@gmail.com>
2021-01-09 10:31:15 +0530

    made some minor changes on polybar

--- fish/config.fish
+++ fish/config.fish
@@ -2,7 +2,7 @@
 set PATH $PATH /home/paradox/.gem/ruby/2.7.0/bin
 set PATH $PATH /home/paradox/.local/bin
 
-set TERM xterm-256color
+
 set -x BAT_THEME Nord
 set -x SPCLI "LD_PRELOAD=/usr/local/lib/spotify-adblock.so spotify"
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+# BSPWM on Arch Linux rice
+
+#work on Readme in is in progress
+
